81b865870d6a30f5581c142e31c7c7eda4f72eee
unknown <alkaffahamed@hotmail.com>
2021-07-25 00:28:08 +0800

    Added MP 1: Ex2 (Contains Errors) - Added Insertion Sort, Bubble Sort and Generate Random Int codes - Added Compiled library.py - Errors: Unable to run the sort and update the results    - Suspect something to do with library.py line 64    - Possible ex2.html may have mistakes

--- mp_sort/app/static/library.py
+++ mp_sort/app/static/library.py
@@ -3,34 +3,37 @@ import random
 
 array = []
 
+
 def gen_random_int(number, seed):
-	pass
+    result = list(range(number))
+    random.shuffle(result, random.seed(seed))
+    return result
+
 
 def generate():
-	global array
+    global array
 
-	number = 10
-	seed = 200
+    number = 10
+    seed = 200
 
-	# call gen_random_int() with the given number and seed
-	# store it to the global variable array
-	pass
+    # call gen_random_int() with the given number and seed
+    # store it to the global variable array
 
-	array = None
-	# convert the items into one single string 
-	# the number should be separated by a comma
-	# and a full stop should end the string.
-	pass
+    array = gen_random_int(number, seed)
+    # convert the items into one single string
+    # the number should be separated by a comma
+    # and a full stop should end the string.
+    pass
 
-	array_str = None
+    array_str = None
 
-	# This line is to placed the string into the HTML
-	# under div section with the id called "generate"	
-	document.getElementById("generate").innerHTML = array_str
+    # This line is to placed the string into the HTML
+    # under div section with the id called "generate"
+    document.getElementById("generate").innerHTML = array_str
 
 
 def sortnumber1():
-	'''	This function is used in Exercise 1.
+    '''	This function is used in Exercise 1.
 		The function is called when the sort button is clicked.
 
 		You need to do the following:
@@ -39,14 +42,15 @@ def sortnumber1():
 		- call your sort function, either bubble sort or insertion sort
 		- create a string of the sorted numbers and store it in array_str
 	'''
-	pass
+    pass
 
-	array_str = None
-	
-	document.getElementById("sorted").innerHTML = array_str
+    array_str = None
+
+    document.getElementById("sorted").innerHTML = array_str
+
 
 def sortnumber2():
-	'''	This function is used in Exercise 2.
+    '''	This function is used in Exercise 2.
 		The function is called when the sort button is clicked.
 
 		You need to do the following:
@@ -56,20 +60,60 @@ def sortnumber2():
 		- call your sort function, either bubble sort or insertion sort
 		- create a string of the sorted numbers and store it in array_str
 	'''
-	# The following line get the value of the text input called "numbers"
-	value = document.getElementsByName("numbers")[0].value
+    # The following line get the value of the text input called "numbers"
+    value = document.getElementByName("numbers").value
 
-	# Throw alert and stop if nothing in the text input
-	if value == "":
-		window.alert("Your textbox is empty")
-		return
+    # Throw alert and stop if nothing in the text input
+    if value == "":
+        window.alert("Your textbox is empty")
+        return
 
-	# Your code should start from here
-	# store the final string to the variable array_str
-	pass
+    # Your code should start from here
+    # store the final string to the variable array_str
 
-	array_str = None
+    array = [i.strip() for i in value.split(',')]
+    # print(array)
+    insertion_sort(array)
+    array_str = ", ".join(array)
+    # print(array_str)
 
-	document.getElementById("sorted").innerHTML = array_str
+    document.getElementById("sorted").innerHTML = array_str
 
 
+# Insertion Sort
+def insertion_sort(array):
+    n = len(array)
+
+    # Outer Loop
+    for i in range(1, n):
+        # Set Inner Loop strating index = Outer Loop
+        j = i
+        # Start the Inner Loop
+        # Loop must break if decrement j all the way till 0 & if previous element > element j
+        while j > 0 and float(array[j]) < float(array[j - 1]):
+            array[j], array[j - 1] = array[j - 1], array[j]
+            j -= 1
+
+
+# Bubble Sort
+def bubble_sort(array):
+    # count = 0
+    n = len(array)
+    isSwap = True
+
+    # While loop replaces the outer for loop
+    while isSwap:
+        isSwap = False
+        lastIndex = 0
+        # print("-----")
+        # Maintaining j as Inner loop iterator
+        for j in range(n - 1):
+            # Swap if next element < current element
+            #count += 1
+            if array[j + 1] < array[j]:
+                array[j + 1], array[j] = array[j], array[j + 1]
+                isSwap = True  # Swapping happened, so unset flag
+                lastIndex = j + 1  # Extract last index + 1 (j + 1) because array is sorted till j+1, not j
+        # print(array, j, n, lastIndex, count)
+        n = lastIndex  # And set as new n for inner loop
+	#return count
