16f4dc838831024e08c633eccf5487711344e070
unknown <alkaffahamed@hotmail.com>
2021-08-12 22:51:59 +0800

    Completed Mini Project 2 - Downloaded and Merged from Jing Lun

--- mp_calc/application.py
+++ mp_calc/application.py
@@ -8,4 +8,4 @@ def make_shell_context():
 			'TimeRecord': TimeRecord}
 
 if __name__ == "__main__":
-	application.run(host="0.0.0.0", port=8080, debug=True)
+	application.run(host="127.0.0.1", port=5000, debug=True)
